280307dc5a075c7defd3c5552d2af4c1289334d9
dengjunjian <741668857@qq.com>
2019-07-29 08:26:51 +0800

    UIButton

--- Testdemo/ViewController.m
+++ Testdemo/ViewController.m
@@ -14,22 +14,34 @@
 
 @implementation ViewController
 
-- (void) createUI{
-    UILabel* label=[[UILabel alloc] init];
-    
-    label.text=@"hallo world";
-    label.frame=CGRectMake(100, 100, 100, 40);
-    label.backgroundColor=[UIColor greenColor];
-    label.textColor=[UIColor blueColor];
-    label.shadowOffset=CGSizeMake(10, 10);
-    label.shadowColor=[UIColor blackColor];
-    label.font=[UIFont systemFontOfSize:15];
-    [self.view addSubview:label];
+-(void) createUIbutton{
+    UIButton* btn=[UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btn.frame=CGRectMake(100, 100, 200 ,100);
+    [btn setTitle:@"abc" forState:UIControlStateNormal];
+    [btn setTitle:@"ppp" forState:UIControlStateHighlighted];
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateHighlighted];
+    btn.titleLabel.font=[UIFont systemFontOfSize:30];
+    [self.view addSubview:btn];
 }
+
+-(void) createImgbtn{
+    UIButton* imgbtn=[UIButton buttonWithType:UIButtonTypeCustom];
+    imgbtn.frame=CGRectMake(70, 200, 300, 300);
+    UIImage* icon1=[UIImage imageNamed:@"UNADJUSTEDNONRAW_thumb_1.jpg"];
+    UIImage* icon2=[UIImage imageNamed:@"UNADJUSTEDNONRAW_thumb_5.jpg"];
+    [imgbtn setImage:icon1 forState:UIControlStateNormal];
+    [imgbtn setImage:icon2 forState:UIControlStateHighlighted];
+    [self.view addSubview:imgbtn];
+    
+    
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self createUI];
+    [self createUIbutton];
+    [self createImgbtn];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
