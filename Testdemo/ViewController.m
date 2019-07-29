3a0886a011f8290b0a39148ec879d896285e9274
dengjunjian <741668857@qq.com>
2019-07-29 08:54:26 +0800

    UIWindow

--- Testdemo/ViewController.m
+++ Testdemo/ViewController.m
@@ -18,30 +18,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIView* view01=[[UIView alloc] init];
-    view01.frame=CGRectMake(100, 100, 200, 200);
-    view01.backgroundColor=[UIColor blueColor];
-    
-    UIView* view02=[[UIView alloc] init];
-    view02.frame=CGRectMake(150, 150, 200, 200);
-    view02.backgroundColor=[UIColor greenColor];
-    
-    UIView* view03=[[UIView alloc] init];
-    view03.frame=CGRectMake(200, 200, 200, 200);
-    view03.backgroundColor=[UIColor blackColor];
-    
-    [self.view addSubview:view02];
-    [self.view addSubview:view03];
-    [self.view addSubview:view01];
-    
-    [self.view bringSubviewToFront:view01];
-    [self.view sendSubviewToBack:view03];
-    
-    
-    UIView* viewfront=self.view.subviews[0];
-    if(viewfront==view03){
-        NSLog(@"hahaha");
-    }
     
 
     // Do any additional setup after loading the view, typically from a nib.
